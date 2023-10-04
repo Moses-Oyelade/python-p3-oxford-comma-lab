bda44aa735e2153c2761ee1d507a5acd6e056b34
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-04 17:38:27 +0100

    two tests passed two failed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,23 @@
+
+
+
 def oxford_comma(items):
-    return None
+    
+    new_list = []
+    for index in range(len(items)):
+        new_list.append(f"{items[index]}," if index < len(items) -1 else f"and {items[index]}")
+        
+    return " ".join(new_list)
+print(oxford_comma(["man", "woman", "girl", "boy"]))
+print(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]))
+print(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]))
+
+
+# def oxford_comma(items: list) -> str:
+
+#     new_list = []
+#     for i in range(len(items)):
+#         new_list.append(f"{items[i]}," if i < len(items) - 1 else f"and {items[i]}")
+        
+#     return " ".join(new_list)
+# print(oxford_comma(["man", "woman", "girl", "boy"]))
